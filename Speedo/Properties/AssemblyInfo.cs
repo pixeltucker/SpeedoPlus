42d4d920faf1c2086d84b001af8e40ffe9b78433
Solal Pirelli <solal.pirelli@gmail.com>
2013-08-05 13:20:01 +0200

    Increased the version number.

--- Speedo/Properties/AssemblyInfo.cs
+++ Speedo/Properties/AssemblyInfo.cs
@@ -7,16 +7,13 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle( "Speedo" )]
 [assembly: AssemblyDescription( "A stylish speedometer for Windows Phone 8" )]
-[assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "Pixel Tucker" )]
 [assembly: AssemblyProduct( "Speedo" )]
 [assembly: AssemblyCopyright( "Copyright © Pixel Tucker Pty Ltd 2011-2013" )]
-[assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
 [assembly: ComVisible( false )]
-[assembly: Guid( "2d145fce-8aa7-41f5-9df3-93f5907daef2" )]
 
-[assembly: AssemblyVersion( "1.1.0.0" )]
-[assembly: AssemblyFileVersion( "1.1.0.0" )]
+[assembly: AssemblyVersion( "1.2.0.0" )]
+[assembly: AssemblyFileVersion( "1.2.0.0" )]
 [assembly: NeutralResourcesLanguageAttribute( "en" )]
